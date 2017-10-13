15a427284c11c82d1259de3af3c8e5839554934e
Dragu Costel <dragu.costel@yahoo.com>
2017-10-13 21:44:02 +0300

    Zip Creator

--- SpringBootRunner/src/main/java/com/enva/controllers/ZipCreator.java
+++ SpringBootRunner/src/main/java/com/enva/controllers/ZipCreator.java
@@ -3,6 +3,12 @@ package com.enva.controllers;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellAddress;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,30 +35,51 @@ public class ZipCreator {
     }
 
     private void createWorbook() throws IOException {
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("FirstSheet");
-        HSSFRow rowhead = sheet.createRow((short) 0);
+        Workbook workbook = new XSSFWorkbook();
+
+        createSheet(workbook, "Sheet 1");
+        createSheet(workbook, "Sheet 2");
+        createSheet(workbook, "Sheet 3");
+        createSheet(workbook, "Sheet 4");
+        createSheet(workbook, "Sheet 5");
+        createSheet(workbook, "Sheet 6");
+        createSheet(workbook, "Sheet 7");
+        createSheet(workbook, "Sheet 8");
+        createSheet(workbook, "Sheet 9");
+
+       workbook.setActiveSheet(2);
+        workbook.setFirstVisibleTab(2);
+
+        workbook.getSheet("Sheet 3").setActiveCell(new CellAddress(0, 0));
+        byte[] fileAsBites = getWbToZipAsBites(getWbAsBites(workbook), "text.xlsx");
+
+        try (FileOutputStream file = new FileOutputStream("D:\\test\\test" + System.currentTimeMillis() + ".zip")) {
+            file.write(fileAsBites);
+        }
+
+        workbook.close();
+    }
+
+    private void createSheet(Workbook workbook, String name) {
+        Sheet sheet = workbook.createSheet(name);
+        sheet.setAutoFilter(new CellRangeAddress(0, 0, 0, 9));
+        Row rowhead = sheet.createRow(0);
         rowhead.createCell(0).setCellValue("No.");
         rowhead.createCell(1).setCellValue("Name");
         rowhead.createCell(2).setCellValue("Address");
         rowhead.createCell(3).setCellValue("Email");
-
-        HSSFRow row = sheet.createRow((short) 1);
-        row.createCell(0).setCellValue("1");
-        row.createCell(1).setCellValue("Sankumarsingh");
-        row.createCell(2).setCellValue("India");
-        row.createCell(3).setCellValue("sankumarsingh@gmail.com");
-
-        workbook.close();
-
-        byte[] fileAsBites = getWbToZipAsBites(getWbAsBites(workbook), "text.xlsx");
-
-        try(FileOutputStream file = new FileOutputStream("D:\\test.zip")) {
-            file.write(fileAsBites);
+        for (int x = 0; x < 1000; ++x) {
+            Row row = sheet.createRow(x);
+            row.createCell(0).setCellValue(x);
+            row.createCell(1).setCellValue("Sankumarsingh " + x);
+            row.createCell(2).setCellValue("India");
+            row.createCell(3).setCellValue("sankumarsingh@gmail.com");
         }
+
+
     }
 
-    private byte[] getWbAsBites(HSSFWorkbook workbook) {
+    private byte[] getWbAsBites(Workbook workbook) {
         ByteArrayOutputStream bus = new ByteArrayOutputStream();
         try {
             workbook.write(bus);
